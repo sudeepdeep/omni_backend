f5959402ac6a030ba67afce6086a28e3aad5dc60
developer deepu <sudeepsudeep340@gmail.com>
2025-03-15 15:41:49 +0530

    auth controller modified

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from '../schema/user.schema';
+import { User, UserSchema } from 'src/schema/user.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './jwt-constants';
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,10 @@ import { LoginDto } from './dto/auth.dto';
 import * as bcrypt from 'bcryptjs';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { IUser } from '../user/interface/user.interface';
-import { User } from '../schema/user.schema';
+import { IUser } from 'src/user/interface/user.interface';
+import { User } from 'src/schema/user.schema';
 import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/user/dtos/user.dto';
 
 @Injectable()
 export class AuthService {
@@ -31,5 +32,58 @@ export class AuthService {
       access_token: await this.jwtService.signAsync(payload),
       userId: user._id,
     };
+  }
+
+  async signUp(body: CreateUserDto) {
+    const { username, email } = body;
+    let user = await this.model.findOne({ username: username, email: email });
+    if (user) {
+      throw new UnprocessableEntityException(
+        'User with the given username/email already exists',
+      );
+    }
+
+    //create user
+    user = await this.model.create(body);
+    const payload = { sub: user._id, username: username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      userId: user._id,
+    };
+  }
+
+  async handleGoogleSignIn(payload: any) {
+    const email = payload.email;
+    const user = await this.model.findOne({ email: email });
+
+    if (!user) {
+      //create an user
+      const data = {
+        email: email,
+        username: email.split('@')[0],
+        authSource: 'google',
+      };
+      try {
+        const userCreated = await this.model.create(data);
+        const tokenData = {
+          sub: userCreated._id,
+          username: userCreated.username,
+        };
+
+        return {
+          access_token: await this.jwtService.signAsync(tokenData),
+          userId: userCreated._id,
+        };
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      //login user
+      const tokenData = { sub: user._id, username: user.username };
+      return {
+        access_token: await this.jwtService.signAsync(tokenData),
+        userId: user._id,
+      };
+    }
   }
 }
